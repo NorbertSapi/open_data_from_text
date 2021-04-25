be53fd07cb342ecc110a7c28f4340b8ac141f6c0
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-04-26 00:58:52 +0100

    3. step: This is a working version of my exercise!!!    - opens the txt file    - assign the values to the list called "store_list"    - Close the txt file    - creates variables for the decoding part.        "number" integer var - stores the incremented elements.        "decoded_list" this is a new list - stores the decoded numbers    - for loop to decode the store_list    - print decoded list

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # This is an exercise to practice how to get data from a text file.
 
-f = open('data_store.txt', 'r+')
-lines = [line for line in f.readlines()]
-f.close()
-print(lines)
+# opens the txt file
+text_file = open('data_store.txt')    # opens the txt file
+
+# assign the values to the list called "store_list"
+store_list = []
+for line in text_file:
+    store_list.append(int(line.rstrip()))
+text_file.close()                     # Close the txt file
+print(store_list)
+
+number = 0                          # creates variables for the decoding part.
+decoded_list = []
+
+for x in store_list:                # for loop to decode the store_list
+    number += x
+    decoded_list.append(number)
+
+print(decoded_list)                 # for loop to decode the store_list
