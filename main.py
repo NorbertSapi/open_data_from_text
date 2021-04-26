0d8818ccbd219280132dbeb05cde8562691aed56
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-04-26 11:56:31 +0100

    3. step: This is a working version of my exercise!!!    - opens the txt file    - assign the values to the list called "store_list"    - Close the txt file    - create decoder function    - creates variables for the decoding part.        "number" integer var - stores the incremented elements.        "decoded_list" this is a new list - stores the decoded numbers    - for loop to decode the store_list    - assign & print decoded list    - open/create a new text file called "final-1.txt"    - write the list in the text file.    - close text file

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 # This is an exercise to practice how to get data from a text file.
 
 # opens the txt file
-text_file = open('data_store.txt')    # opens the txt file
-
+text_file = open('data_store.txt')  # opens the txt file
+print(type(text_file))
 # assign the values to the list called "store_list"
 store_list = []
+
 for line in text_file:
     store_list.append(int(line.rstrip()))
-text_file.close()                     # Close the txt file
-print(store_list)
+# Close the txt file
+text_file.close()  # Close the txt file
+print()
 
-number = 0                          # creates variables for the decoding part.
-decoded_list = []
 
-for x in store_list:                # for loop to decode the store_list
-    number += x
-    decoded_list.append(number)
+# create a function
+def decoder(test_list):
+    number = 0  # creates variables for the decoding part.
+    decoded_list = []
+    for x in test_list:  # for loop to decode the store_list
+        number += x
+        decoded_list.append(number)
+    return decoded_list
 
-print(decoded_list)                 # for loop to decode the store_list
+
+# assign and display the decoded list
+decoded_list_1 = decoder(store_list)  # assign the decoded list
+print(decoded_list_1)
+
+
+f = open("final-1.txt", "a+")  # create a new txt file for the final result
+# write the decoded_list_1 in the text file.
+for i in decoded_list_1:
+    f.write("%s " % i)
+f.close()
